108b8f8393a96fb9c853614ca3c2223cc8d14f1c
Vaibhav1323 <87201046+Vaibhav1323@users.noreply.github.com>
2024-08-23 22:29:28 +0530

    Create HelloWorld.java

---
+++ Day (1)/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int x = 10;
+        double y = 20.5;
+        String name = "John";
+
+        // Print variables
+        System.out.println("Hello, " + name + "!");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
