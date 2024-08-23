0d75cf0a697c26e9c731b45e4678cac3464addc2
Vaibhav1323 <87201046+Vaibhav1323@users.noreply.github.com>
2024-08-23 23:04:26 +0530

    Create SumOfNumbers.java

---
+++ Day (5)/SumOfNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        // Use a for loop to calculate the sum of numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of numbers from 1 to 10: " + sum);
+    }
+}
